812539553ac89c24f46e8625426c6c49e1ae55f7
Jason Perlman <email": null>
2016-11-17 15:01:07 -0500

    Bolded Key Phrases, Materialize CSS for Sentiment Bar

--- display.js
+++ display.js
@@ -9,13 +9,30 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 
 function doStuff(data){
+  console.log(data)
   var keyPhrases = data.keyPhrases.documents[0].keyPhrases;
   var text = data.text;
   for(var i=0;i<keyPhrases.length;i++)
   {
-    var replacementText='<mark>'.concat(keyPhrases[i]).concat('</mark>');
+    var replacementText='<bolded>'.concat(keyPhrases[i]).concat('</bolded>');
     text = text.replace(keyPhrases[i], replacementText);
   }
   $('#container').append(text);
+  colorObjectLiteral={
+    25:"Red",
+    50:"Grey",
+    75:"Blue",
+    100:"Green"
+  }
+  var barColor;
+  var percent = Math.round(data.sentiment.documents[0].score * 100.0);
+    if (percent<25){barColor="red"}
+    else if (percent<50) {barColor="grey"}
+    else if (percent<75) {barColor="blue"}
+    else{barColor="green"}
 
+  var x = ('<div class="progress">\
+      <div class="determinate '+barColor+'" style="width:'+ percent + '%"></div>\
+  </div>')
+  $('#container').append(x);
 }
